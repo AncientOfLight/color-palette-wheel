b57af68e101a8cfbdabe5ad8d0cc0c4ca2e27425
AncientOfLight <lotso_eduardo@hotmail.com>
2026-06-05 06:58:25 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/js-plugin-react'; // o el plugin que use tu app
 
 export default defineConfig({
+  base: '/luxpalette/', // <-- ¡ESTA LÍNEA ES EL ESCUDO QUE CURA LA PANTALLA BLANCA!
   plugins: [react()],
-base: '/luxpalette/',
-})
+});
