fa02c47ed0a144877eec1d4b1e1ec72f2218a8b7
AncientOfLight <lotso_eduardo@hotmail.com>
2026-06-05 06:59:40 -0600

    Remove comment from vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/js-plugin-react'; // o el plugin que use tu app
+import react from '@vitejs/js-plugin-react'; 
 
 export default defineConfig({
-  base: '/luxpalette/', // <-- ¡ESTA LÍNEA ES EL ESCUDO QUE CURA LA PANTALLA BLANCA!
-  plugins: [react()],
+  base: '/luxpalette/', 
 });
